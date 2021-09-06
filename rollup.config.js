5ae190ce7028e0d2581e34ba886fce9970dfc678
arista <arista@abramsons.info>
2021-09-06 10:50:17 -0400

    Added rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
 import typescript from "rollup-plugin-typescript2"
+import commonjs from "@rollup/plugin-commonjs"
+import resolve from "@rollup/plugin-node-resolve"
 import pkg from "./package.json"
 
 const extensions = [".js", ".jsx", ".ts", ".tsx"]
 const input = "src/index.ts"
 
 const plugins = [
+  commonjs(),
   typescript({
     typescript: require('typescript'),
     clean: true
@@ -29,5 +32,25 @@ export default [
       sourcemap: false,
     },
     plugins,
+  },
+  // Browser-loadable file
+  {
+    input,
+    output: [
+      {
+        file: "dist/Application.js",
+        name: "Application",
+        format: "iife",
+        sourcemap: true,
+        globals: {
+        },
+      },
+    ],
+    external: [
+    ],
+    plugins: [
+        ...plugins,
+      resolve(),
+    ],
   },
 ]
